67645f345770c1277c0a5ccd3bd2170d66b0d81a
sunwoo0706 <sunwoo0706@icloud.com>
2021-12-31 22:45:18 +0900

    Add NumberList component

---
+++ src/components/NumberList/container.ts
@@ -0,0 +1,33 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+import { getDuration } from "src/utils/utils";
+
+export interface NumberListWrapperProps {
+    idx: number;
+    time: number;
+    length: number;
+}
+
+export interface NumberListProps {
+    start: number;
+    idx: number;
+    time: number;
+    length: number;
+}
+
+const NumberListSlide = keyframes`
+    0% {
+        transform: translateY(0);
+    }
+    100% {
+        transform: translateY(-9rem);
+    }
+`;
+
+export const NumberListWrapper = styled.ul<NumberListWrapperProps>`
+    width: fit-content;
+    text-align: center;
+    animation: ${NumberListSlide} ease forwards;
+    animation-duration: ${({idx, time, length}) => getDuration(idx, time, length)};
+`;
